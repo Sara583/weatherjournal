926381160120cec512cc38403a66c3e3d6758e8e
Sera Mohamed <77129891+Sara583@users.noreply.github.com>
2022-01-12 21:18:28 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -2,19 +2,21 @@
 let zipNumber = document.getElementById('zip');
 let feel = document.querySelector('#feelings');
 let generatingBtn = document.querySelector('#generate');
-const api = "&appid=b63fc12263da2ed25920df95b431c154";
-let unit = "&units=metric";
+const api = "&appid=b63fc12263da2ed25920df95b431c154&units=metric";
 let url = "https://api.openweathermap.org/data/2.5/weather?zip=";
 
 //Async await functions to fetch data from API
 const wData = async () => {
   // Create a new date instance dynamically with JS
   let d = new Date();
-  let newDate = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
+  let month = d.getMonth() + 1
+  let newDate = (month + '/' + d.getDate() + '/' + d.getFullYear());
   let enterdZip = zipNumber.value;
+  //ternary operator to check validity of the enterd zip code
   (enterdZip.length == 5) ? console.log("successed"): alert("zip codes must be 5 numbers");
-  let finalUrl = url + enterdZip + unit + api;
+  let finalUrl = url + enterdZip + api ;
   let response = await fetch(finalUrl);
+  console.log(finalUrl);
   const data = await response.json();
   let temp = data.main.temp;
   let content = (document.querySelector('#feelings')).value;
@@ -23,12 +25,24 @@ const wData = async () => {
     date: newDate,
     content: content
   });
-  //ternary operator to check validity of the enterd zip code
+  // async function to update the fetched data on screen
+  async function ui() {
+    let respond = await fetch('/all');
+    try {
+      const newData = await respond.json();
+      document.querySelector('#date').innerText = `Date: ${(newData.date)}`;
+      document.getElementById('temp').innerText = `Temperature: ${newData.temp} Celisus Degrees`;
+      document.querySelector('#content').innerText = `I feel like: ${newData.content}`;
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  await ui();
 
 }
-// adding event listener to the generating button on double click
-generatingBtn.addEventListener('dblclick', wData);
-//post request
+// adding event listener to the generating button onclick
+generatingBtn.addEventListener('click', wData);
+//post request to server side
 const postData = async (url = '', projectData = {}) => {
   const response = await fetch(url, {
     method: 'POST',
@@ -39,29 +53,11 @@ const postData = async (url = '', projectData = {}) => {
     // Body data type must match "Content-Type" header
     body: JSON.stringify(projectData),
   });
-
   console.log(projectData);
   try {
-
     const newData = await response.json();
     console.log("response", newData);
-    ui();
-
-
   } catch (e) {
     console.log("error", e);
-
   };
-  // async function to update the fetched data on screen
-  async function ui() {
-    let respond = await fetch('/all');
-    try {
-      const newData = await respond.json();
-      document.querySelector('#date').innerText = `Date: ${newData.date}`;
-      document.getElementById('temp').innerText = `Temperature: ${newData.temp}`;
-      document.querySelector('#content').innerText = `I feel like: ${newData.content}`;
-    } catch (e) {
-      console.log(e)
-    }
-  }
 }
